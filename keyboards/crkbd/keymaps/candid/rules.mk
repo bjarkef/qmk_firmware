db4026acf2a2904c30da949addd68a738364f23f
Bjarke Freund-Hansen <bjarkefh@gmail.com>
2022-01-29 14:17:24 +0100

    Custom DK keymap based on soundmonster

---
+++ keyboards/crkbd/keymaps/candid/rules.mk
@@ -0,0 +1,9 @@
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = no
+MOUSEKEY_ENABLE    = yes
+NKRO_ENABLE        = yes
+OLED_ENABLE        = yes
+OLED_DRIVER        = SSD1306
+LTO_ENABLE         = yes
+WPM_ENABLE         = yes
+
